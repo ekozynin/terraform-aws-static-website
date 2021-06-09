c2f9d26d8b7607518362a17804b63987d62af647
Eugen Kozynin <ekozynin@gmail.com>
2021-06-09 22:46:30 +1000

    updated version if certificate dependency

--- certificate.tf
+++ certificate.tf
@@ -1,6 +1,6 @@
 module "www_certificate" {
   source = "orinta-com/ssl-certificate/aws"
-  version = "0.0.4"
+  version = "~> 1.0.0"
   providers = {
     // Cloudfront only supports ACM certs issues in us-east-1
     aws = aws.cloudfront
